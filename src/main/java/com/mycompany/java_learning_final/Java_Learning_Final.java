0243b1ea3634e2bb6791fe9a4b1822d746a7652e
JavierGD15 <javiergdcontactgt@gmail.com>
2022-11-11 17:10:35 -0600

    Ejemplo

--- src/main/java/com/mycompany/java_learning_final/Java_Learning_Final.java
+++ src/main/java/com/mycompany/java_learning_final/Java_Learning_Final.java
@@ -5,18 +5,15 @@ import java.util.Scanner;
 public class Java_Learning_Final {
 
     public static void main(String[] args) {
-        
         Scanner sp = new Scanner(System.in);
         System.out.println("Ingrese su radio");
-        
-        float radio  = sp.nextFloat();
-        
+
+        float radio = sp.nextFloat();
+
         System.out.println("El area es: "
-        +(Math.PI *Math.pow(radio,2)));
-        
+                + (Math.PI * Math.pow(radio, 2)));
+
         System.out.println("Longitud de circunferencia es: "
-                +(2*Math.PI*radio));
-        
-        
+                + (2 * Math.PI * radio));
     }
 }
